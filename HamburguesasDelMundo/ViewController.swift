07d621cbb9559564b9bf85d15b76afe3bb61fa06
Sebastià Fiol <sfiol@efeefe.com>
2016-04-24 11:18:12 +0200

    Initial Commit

---
+++ HamburguesasDelMundo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HamburguesasDelMundo
+//
+//  Created by SebasFiol on 24/4/16.
+//  Copyright © 2016 SebasFiol. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
